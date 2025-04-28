b90d9d1f250586e2e1ba56d27109fe47adf56a71
An Thanh Le <an.thanhle@infodation.vn>
2025-04-28 21:26:30 +0700

    Structure API Folder

--- VPhimTV_BE/routes/web.php
+++ VPhimTV_BE/routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::get('/login', [AuthController::class, 'showlogin']);
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/register', [AuthController::class, 'showRegister']);
-Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::get('/logout', [AuthController::class, 'logout']);
+require __DIR__ . '/web/home.php';
+require __DIR__ . '/web/admin.php';
+require __DIR__ . '/web/auth.php';
---
+++ VPhimTV_BE/routes/web/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showlogin']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::get('/register', [AuthController::class, 'showRegister']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::get('/logout', [AuthController::class, 'logout']);
