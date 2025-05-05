e6f83ce1b03b6702ec5623a41654cd4b7acd475f
An Thanh Le <an.thanhle@infodation.vn>
2025-05-05 16:52:44 +0700

    Add database

---
+++ VPhimTV_BE/database/migrations/2025_05_05_091851_create_episodes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('movie_id')->nullable();
+            $table->string('server_name')->nullable();
+            $table->string('episode_name')->nullable();
+            $table->string('slug')->nullable();
+            $table->text('file_name')->nullable();
+            $table->text('link_embed')->nullable();
+            $table->text('link_m3u8')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('episodes');
+    }
+};
