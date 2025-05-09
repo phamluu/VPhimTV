585116cf97d32febeed5aa7e2382e107399ce47d
An Thanh Le <an.thanhle@infodation.vn>
2025-05-09 17:21:39 +0700

    Update movie categories table

--- VPhimTV_BE/database/migrations/2025_05_05_091851_create_movie_categories_table.php
+++ VPhimTV_BE/database/migrations/2025_05_05_091851_create_movie_categories_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('movie_categories', function (Blueprint $table) {
             $table->id();
+            $table->integer('movie_id')->nullable();
+            $table->integer('category_id')->nullable();
             $table->timestamps();
         });
     }
