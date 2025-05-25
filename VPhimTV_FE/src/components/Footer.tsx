6a3eed653c1cb4d538eac8effc7ee194e29c556b
An Thanh Le <an.thanhle@infodation.vn>
2025-05-25 12:36:40 +0700

    Update New Footer and Header

---
+++ VPhimTV_FE/src/components/Footer.tsx
@@ -0,0 +1,164 @@
+export default function Footer() {
+  return (
+    <footer className="footer footer-vertical sm:footer-horizontal bg-base-200 text-base-content py-10">
+      <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-5 gap-8">
+        <div className="md:col-span-1">
+          <div className="flex items-center mb-4">
+            <div className="w-10 h-10 bg-primary flex items-center justify-center rounded-full mr-3 text-white">🤟</div>
+            <div>
+              <h2 className="font-bold text-lg">VPhimTV</h2>
+              <p className="text-sm">Phim Mới and Chill</p>
+            </div>
+          </div>
+          <p className="text-sm leading-relaxed">
+            Website xem phim trực tuyến chất lượng cao, cập nhật phim mới nhất vietsub mỗi ngày, xem miễn phí hàng nghìn
+            bộ phim HD/4K đa thể loại.
+          </p>
+        </div>
+        <div>
+          <span className="footer-title text-primary">Phim Mới</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <a href="#" className="link link-hover">
+                Phim chiếu rạp
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim lẻ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim bộ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim hành động
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim viễn tưởng
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim tâm lý
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim hài hước
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <span className="footer-title text-primary">Phim Hay</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <a href="#" className="link link-hover">
+                Phim Mỹ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim Hàn Quốc
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim Trung Quốc
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim Thái Lan
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim Việt Nam
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim Ma Kinh Dị
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phim Hoạt Hình
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <span className="footer-title text-primary">Phim Hot</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <a href="#" className="link link-hover">
+                Về chúng tôi
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Phimmoi
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Sitemap
+              </a>
+            </li>
+          </ul>
+
+          <span className="footer-title text-primary mt-6">Trợ giúp</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <a href="#" className="link link-hover">
+                Hỏi đáp
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Liên hệ
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Tin tức
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <span className="footer-title text-primary">Thông tin</span>
+          <ul className="space-y-2 text-sm">
+            <li>
+              <a href="#" className="link link-hover">
+                Điều khoản sử dụng
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Chính sách riêng tư
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link link-hover">
+                Khiếu nại bản quyền
+              </a>
+            </li>
+          </ul>
+          <p className="text-xs mt-6">© 2025 VPhimTV.Com</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ VPhimTV_FE/src/components/Header.tsx
@@ -0,0 +1,181 @@
+import { useQueries } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+
+import { fetchCategory } from '~/service/category/categoryApi';
+import { fetchCountry } from '~/service/country/countryApi';
+
+export default function Header() {
+  const [value, setValue] = useState('');
+  const navigate = useNavigate();
+  const [searchParams, _setSearchParams] = useSearchParams();
+
+  const [category, country] = useQueries({
+    queries: [
+      {
+        queryKey: ['category'],
+        queryFn: () => fetchCategory(),
+      },
+      {
+        queryKey: ['country'],
+        queryFn: () => fetchCountry(),
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const q = searchParams.get('tu-khoa');
+    if (q) setValue(q);
+  }, [searchParams]);
+
+  const handleSearch = (e: any) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      navigate(`/tim-kiem?tu-khoa=${encodeURIComponent(value.trim())}`);
+    }
+  };
+
+  return (
+    <div className="bg-base-100">
+      <div className="container navbar shadow mx-auto">
+        <div className="sm:hidden flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current"
+            >
+              {' '}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>{' '}
+            </svg>
+          </button>
+        </div>
+
+        <div className="navbar-start">
+          <Link className="btn btn-ghost text-2xl" to="/">
+            VPhimTV.com
+          </Link>
+
+          <div className="inline-flex">
+            <Link
+              className="btn btn-link !no-underline text-nowrap text-base-content hover:text-primary sm:inline-flex hidden"
+              to="/danh-sach/phim-moi?trang=1"
+            >
+              Phim Mới
+            </Link>
+            <Link
+              className="btn btn-link !no-underline text-nowrap text-base-content hover:text-primary sm:inline-flex hidden"
+              to="/danh-sach/phim-le?trang=1"
+            >
+              Phim Lẻ
+            </Link>
+
+            <Link
+              className="btn btn-link !no-underline text-nowrap text-base-content hover:text-primary lg:inline-flex hidden"
+              to="/danh-sach/phim-bo?trang=1"
+            >
+              Phim Bộ
+            </Link>
+
+            <Link
+              className="btn btn-link !no-underline text-nowrap text-base-content hover:text-primary lg:inline-flex hidden"
+              to="/danh-sach/tv-shows?trang=1"
+            >
+              TV Shows
+            </Link>
+
+            <Link
+              className="btn btn-link !no-underline text-nowrap text-base-content hover:text-primary lg:inline-flex hidden"
+              to="/list/hoat-hinh?trang=1"
+            >
+              Hoạt Hình
+            </Link>
+
+            <div className="dropdown dropdown-end dropdown-hover xl:inline-block hidden">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-link !no-underline text-nowrap text-base-content hover:text-primary"
+              >
+                Thể Loại
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu grid grid-cols-3 gap-1 bg-base-100 rounded-box z-1 w-96 p-2 shadow"
+              >
+                {category.data?.map((item: any) => (
+                  <li key={item._id} className="hover:text-primary">
+                    <Link to={`/tim-kiem?the-loai=${item.slug}`}>{item.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="dropdown dropdown-end dropdown-hover xl:inline-block hidden">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-link !no-underline text-nowrap text-base-content hover:text-primary"
+              >
+                Quốc Gia
+              </div>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu grid grid-cols-3 gap-1 bg-base-100 rounded-box z-1 w-96 p-2 shadow"
+              >
+                {country.data?.map((item: any) => (
+                  <li key={item._id} className="hover:text-primary">
+                    <Link to={`/tim-kiem?quoc-gia=${item.slug}`}>{item.name}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-end space-x-3">
+          <label className="input w-20 sm:w-auto">
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={(e) => setValue(e.target.value)}
+              value={value}
+              onKeyDown={handleSearch}
+            />
+            <i className="fa-solid fa-magnifying-glass"></i>
+          </label>
+
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
